464c1051ce8fc4a2e9a73449eaf5df8927c580a2
Marina Goto <5m.clover3@gmail.com>
2018-06-20 17:32:34 +0900

    1

--- TechPod/ViewController.swift
+++ TechPod/ViewController.swift
@@ -8,16 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //StoryBoardで扱うTableViewを宣言
+    @IBOutlet var table: UITableView!
+    
+    //曲名を入れるための配列
+    var songNameArray = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //テーブルビューのデータソースメソッドはViewControllerクラスに書く
+        table.dataSource = self
+        
+        //テーブルビューのデリゲートメソッドはViewControllerメソッドに書く
+        table.delegate = self
+        
+        //songNameArrayに曲名を入れていく
+        songNameArray = ["カレン","エリーゼのために","G線上のアリア"]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //セルの数を設定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //セルの数をsongNameArrayの数にする
+        return songNameArray.count
+    }
+    
+    //ID付きのセルを取得して、セル付属のtextLabelに「テスト」と表示させてみる
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        //セルにsongNameArrayの曲名を表示する
+        cell?.textLabel?.text = songNameArray[indexPath.row]
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(songNameArray[indexPath.row])が選ばれました！")
     }
 
 
